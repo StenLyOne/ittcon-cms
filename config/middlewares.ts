5fe44bac56a66846f95fea0af1e86015d2b1d214
Vlad <macm3@macs-MacBook-Pro.local>
2025-09-02 11:38:29 -0700

    fixed plugins.ts and middlewares.ts

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,9 +2,24 @@ export default [
   "strapi::logger",
   "strapi::errors",
   {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          // чтобы работали превью/иконки/видео с Cloudinary
+          "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          // на всякий случай
+          "connect-src": ["'self'", "https:", "http:"],
+        },
+      },
+    },
+  },
+  {
     name: "strapi::cors",
     config: {
-      origin: ["http://localhost:3000"], // адрес твоего Next.js
+      origin: ["https://ittcon.vercel.app/"], // адрес твоего Next.js
       headers: "*",
       methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
       keepHeaderOnError: true,
